afdd1d65d4b8bab5b44c6fdb366d466ba5f6c669
Subrata Malik <subratamalik1997@gmail.com>
2019-10-15 16:26:37 +0530

    implement audio streaming

--- app/src/main/java/com/example/vishwanandini/vishwa_articles.java
+++ app/src/main/java/com/example/vishwanandini/vishwa_articles.java
@@ -3,7 +3,10 @@ package com.example.vishwanandini;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -13,6 +16,7 @@ public class vishwa_articles extends AppCompatActivity {
     TabLayout tablayout;
     ViewPager viewPager;
     PageAdapter pageAdapter;
+    String head;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +39,30 @@ public class vishwa_articles extends AppCompatActivity {
         tablayout.setupWithViewPager(viewPager);
 
 
+        head=getIntent().getExtras().getString("head");
+
+
+
 //        String id=getIntent().getExtras().getString("head");
 //        Toast.makeText(this, ""+id, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.refresh_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+        if (id==R.id.action_refresh){
+            Toast.makeText(this, "Refreshing...", Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(getApplicationContext(),vishwa_articles.class);
+            intent.putExtra("head",head);
+            startActivity(intent);
+            overridePendingTransition(0,0);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
