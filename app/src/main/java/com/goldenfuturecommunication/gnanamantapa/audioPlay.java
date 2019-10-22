f5d5f6ccc0551fa458361bd9a418a39f63348ae8
Subrata Malik <subratamalik1997@gmail.com>
2019-10-22 13:32:02 +0530

    language changed

--- app/src/main/java/com/goldenfuturecommunication/gnanamantapa/audioPlay.java
+++ app/src/main/java/com/goldenfuturecommunication/gnanamantapa/audioPlay.java
@@ -1,17 +1,40 @@
 package com.goldenfuturecommunication.gnanamantapa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class audioPlay extends AppCompatActivity {
 TextView ct,cb;
@@ -21,6 +44,13 @@ String pt,pb,pa;
     private MediaPlayer mediaPlayer;
     private ProgressDialog progressDialog;
     private boolean initialStage = true;
+
+    //Progress Dialog;
+    private ProgressDialog pDialog;
+    // Progress dialog type (0 - for Horizontal progress bar)
+    public static final int progress_bar_type = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +78,9 @@ String pt,pb,pa;
             @Override
             public void onClick(View view) {
                 if (!playPause) {
-                       // audioStream.setImageResource(R.drawable.ic_pause_circle_outline_black_24dp);
 
                         if (initialStage) {
-                            new Player().execute(pa);
+                            new Player().execute("https://vp254.co.ke/vishwa/audio/"+pa);
                         } else {
                             if (!mediaPlayer.isPlaying())
                                 mediaPlayer.start();
@@ -70,6 +99,33 @@ String pt,pb,pa;
                     }
             }
         });
+
+
+
+        downloadaudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check if SD card is present or not
+                if (CheckForSDCard.isSDCardPresent()) {
+
+                    //check if app has permission to write to the external storage.
+                    int permissionCheke = ContextCompat.checkSelfPermission(audioPlay.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    if (permissionCheke == PackageManager.PERMISSION_GRANTED) {
+                        //Get the URL entered
+                        new DownloadFile().execute("https://vp254.co.ke/vishwa/audio/"+pa);
+                    }else {
+                        ActivityCompat.requestPermissions(audioPlay.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SD Card not found", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        });
+
 
 
     }
@@ -133,5 +189,138 @@ String pt,pb,pa;
             mediaPlayer = null;
         }
     }
+
+
+    //Method to Check If SD Card is mounted or not
+    public static class CheckForSDCard {
+    public static boolean isSDCardPresent() {
+        if (Environment.getExternalStorageState().equals(
+
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+//download task
+private class DownloadFile extends AsyncTask<String, String, String> {
+    private ProgressDialog progressDialog;
+    private String fileName;
+    private String folder;
+    private boolean isDownloaded;
+
+    /**
+     * Before starting background thread
+     * Show Progress Bar Dialog
+     */
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.progressDialog = new ProgressDialog(audioPlay.this);
+        this.progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        this.progressDialog.setCancelable(false);
+        this.progressDialog.show();
+    }
+
+    /**
+     * Downloading file in background thread
+     */
+    @Override
+    protected String doInBackground(String... f_url) {
+        int count;
+        try {
+            URL url = new URL(f_url[0]);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            // getting file length
+            int lengthOfFile = connection.getContentLength();
+
+
+            // input stream to read file - with 8k buffer
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+
+            //Extract file name from URL
+            fileName = f_url[0].substring(f_url[0].lastIndexOf('/') + 1, f_url[0].length());
+
+            //Append timestamp to file name
+            fileName = timestamp + "_" + fileName;
+
+            //External directory path to save file
+            folder = Environment.getExternalStorageDirectory() + File.separator + "Gnana Mantapa";
+
+            File directory = new File(folder);
+
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            // Output stream to write file
+            OutputStream output = new FileOutputStream(folder + fileName);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+
+            while ((count = input.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                // After this onProgressUpdate will be called
+                publishProgress("" + (int) ((total * 100) / lengthOfFile));
+
+                // writing data to file
+                output.write(data, 0, count);
+            }
+
+            // flushing output
+            output.flush();
+
+            // closing streams
+            output.close();
+            input.close();
+            return "Downloaded at: " + folder;// + fileName;
+
+        } catch (Exception e) {
+            Log.e("Error: ", e.getMessage());
+        }
+
+        return "Slow internet connection, try again";
+    }
+
+    /**
+     * Updating progress bar
+     */
+    protected void onProgressUpdate(String... progress) {
+        // setting progress percentage
+        progressDialog.setProgress(Integer.parseInt(progress[0]));
+    }
+
+    @Override
+    protected void onPostExecute(String message) {
+        // dismiss the dialog after the file was downloaded
+        this.progressDialog.dismiss();
+
+        // Display File path after downloading
+        Toast.makeText(getApplicationContext(),
+                message, Toast.LENGTH_LONG).show();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 }
